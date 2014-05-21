c1187e81473728b6047dd311302cfea1095e39c1
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-05-21 09:57:16 +0700

    Version 0.0.3

--- lib/signed_parameters.rb
+++ lib/signed_parameters.rb
@@ -2,11 +2,15 @@ require 'signed_parameters/version'
 require 'signed_parameters/builder'
 
 module SignedParameters
-  def self.to_query(parameters, secret)
-    Builder.new(parameters, secret).to_query
-  end
-
   def self.sign(parameters, secret)
     Builder.new(parameters, secret).sign
+  end
+
+  def self.parameters_with_sign(parameters, sign)
+    Builder.new(parameters, secret).parameters_with_sign
+  end
+
+  def self.to_query(parameters, secret)
+    Builder.new(parameters, secret).to_query
   end
 end
